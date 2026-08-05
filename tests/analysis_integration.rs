0f2f250fdb2a017d09aa944749612a0c7de14e2a
Vendeta <VZVendeta@hotmail.com>
2026-08-05 13:49:29 +0300

    Update analysis_integration.rs

--- tests/analysis_integration.rs
+++ tests/analysis_integration.rs
@@ -2,8 +2,8 @@ use vend_ast::{analyze_program, AliasDecl, FunctionDecl, Program, RegisterWidth,
 
 #[test]
 fn program_level_analysis_reports_conflicts() {
-    let a1 = AliasDecl { name: "a".into(), width: RegisterWidth::Reg64, physical: PhysicalRegister::RAX };
-    let a2 = AliasDecl { name: "b".into(), width: RegisterWidth::Reg32, physical: PhysicalRegister::EAX };
+    let a1 = AliasDecl { name: "a".into(), width: RegisterWidth::Reg64, physical: PhysicalRegister::RAX, span: None };
+    let a2 = AliasDecl { name: "b".into(), width: RegisterWidth::Reg32, physical: PhysicalRegister::EAX, span: None };
 
     let body = ScopeBlock { statements: vec![Statement::AliasDecl(a1), Statement::AliasDecl(a2)] };
 
@@ -11,5 +11,5 @@ fn program_level_analysis_reports_conflicts() {
 
     let diags = analyze_program(&prog);
     assert!(!diags.is_empty());
-    assert!(diags[0].0.contains("In function 'main'"));
+    assert!(diags[0].message.contains("In function 'main'"));
 }
